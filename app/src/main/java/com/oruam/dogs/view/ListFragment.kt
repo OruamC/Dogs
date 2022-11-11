0b9bb3f6e56f52c4f24495d753e2c2781337978c
Oruam Motta <62652819+OruamC@users.noreply.github.com>
2022-11-11 14:15:23 -0300

    Adding navigation safe args

--- app/src/main/java/com/oruam/dogs/view/ListFragment.kt
+++ app/src/main/java/com/oruam/dogs/view/ListFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigation
 import com.oruam.dogs.R
+import com.oruam.dogs.databinding.FragmentListBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -14,11 +17,23 @@ import com.oruam.dogs.R
  */
 class ListFragment : Fragment() {
 
+    private var binding: FragmentListBinding? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        binding = FragmentListBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding?.btnDetails?.setOnClickListener {
+            val action = ListFragmentDirections.actionDetailFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 }

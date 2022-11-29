b1f63ce732936a4533fab00185b2e5b64735dffd
Oruam Motta <62652819+OruamC@users.noreply.github.com>
2022-11-29 11:49:43 -0300

    Sharing informations

--- app/src/main/java/com/oruam/dogs/view/DetailFragment.kt
+++ app/src/main/java/com/oruam/dogs/view/DetailFragment.kt
@@ -110,7 +110,16 @@ class DetailFragment : Fragment() {
                 (activity as MainActivity).checkSmsPermission()
             }
             R.id.action_share -> {
-
+                // Generic flag to share informations to other applications
+                val intent = Intent(Intent.ACTION_SEND)
+                intent.type = "text/plain"
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Check out this dog breed")
+                intent.putExtra(
+                    Intent.EXTRA_TEXT,
+                    "${currentDog?.dogBreed} bred for ${currentDog?.bredFor}"
+                )
+                intent.putExtra(Intent.EXTRA_STREAM, currentDog?.imageUrl)
+                startActivity(Intent.createChooser(intent, "Share with"))
             }
         }
         return super.onContextItemSelected(item)
